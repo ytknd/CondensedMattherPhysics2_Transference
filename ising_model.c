91018b0473c3520cd0ac5810f8a79375cc04f577
Y. Kondo <s2270020@ipc.fukushima-u.ac.jp>
2022-12-14 17:23:36 +0900

    modified:   ising_model.c

--- ising_model.c
+++ ising_model.c
@@ -4,63 +4,62 @@
 #include <math.h>
 #include "MT.h"
 
+
 int main (void) {
-    int screen = 2, cls = 3, m = 10, b = 0, im = m * m, ax0 = 0, ay0 = 100, ss, sf, s0, s1, sum_spin;
-    int jp1, jm1, ip1, im1, imax = 10, jmax = 10;
-    int s[20][20], r[5][2];
-    double kt = 10, ex, random;
+    int spin_state, sf, sum_spin = 0, mesh = 10, monte_carlo_step = 500;
+    int spin[mesh][mesh];
+    double kt = 0.01, ex, random;
 
     init_genrand ((unsigned) time (NULL));
 
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < m; j++) {
-            ss = 1;
+    for (int i = 0; i < mesh; i++) {
+        for (int j = 0; j < mesh; j++) {
+            spin_state = 1;
 
             if (genrand_real1() > 0.5) {
-                ss = -1;
+                spin_state = -1;
             }
 
-            s[i][j] = ss;
+            spin[i][j] = spin_state;
         }
     }
 
+    for (int i = 0; i < mesh; i++) {
+        for (int j = 0; j < mesh; j++) {
 
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < m; j++) {
-
-            s[0][j] = 1;
-            s[m -1][j] = 1;
-            s[i][0] = 1;
-            s[i][m -1] = 1;
+            spin[0][j] = 0;
+            spin[mesh -1][j] = 0;
+            spin[i][0] = 0;
+            spin[i][mesh -1] = 0;
         }
     }
 
-    for (int kk = 0; kk < 500; kk++) {
-        for (int i= 1; i < m - 1; i++) {
-            for(int j = 1; j < m - 1; j++) {
-                sf = 2 * s[i][j] * (s[i - 1][j] + s[i + 1][j] + s[i][j - 1] + s[i][j + 1]);
+    for (int kk = 0; kk < monte_carlo_step; kk++) {
+        for (int i= 1; i < mesh - 1; i++) {
+            for(int j = 1; j < mesh - 1; j++) {
+                sf = 2 * spin[i][j] * (spin[i - 1][j] + spin[i + 1][j] + spin[i][j - 1] + spin[i][j + 1]);
 
                 ex = exp(-sf / kt);
                 random = genrand_real1();
 
                 if (ex > random) {
-                    s[i][j] *= -1;
+                    spin[i][j] *= -1;
                 }
             }
         }
     }
 
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < m; j++) {
-            printf ("%d\t", s[i][j]);
+    for (int i = 0; i < mesh; i++) {
+        for (int j = 0; j < mesh; j++) {
+            printf ("%d\t", spin[i][j]);
         }
         printf ("\n");
     }
     printf ("\n");
 
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < m; j++) {
-            sum_spin += s[i][j];
+    for (int i = 0; i < mesh; i++) {
+        for (int j = 0; j < mesh; j++) {
+            sum_spin += spin[i][j];
         }
     }
     printf ("%d\n", sum_spin);
